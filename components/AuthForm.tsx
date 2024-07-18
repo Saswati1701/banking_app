4bec1659ea919ce3abca1c1377422411c1e58390
Saswati1701 <saswatiisabella2002@gmail.com>
2024-07-18 08:48:26 +0530

    sign up form implemented, pushed sign up user to db

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,7 +23,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
-import PlaidLink from './PlaidLink';
+// import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -68,12 +68,12 @@ const AuthForm = ({ type }: { type: string }) => {
         }
 
         if(type === 'sign-in') {
-          const response = await signIn({
-            email: data.email,
-            password: data.password,
-          })
+          // const response = await signIn({
+          //   email: data.email,
+          //   password: data.password,
+          // })
 
-          if(response) router.push('/')
+          // if(response) router.push('/')
         }
       } catch (error) {
         console.log(error);
@@ -114,7 +114,7 @@ const AuthForm = ({ type }: { type: string }) => {
       </header>
       {user ? (
         <div className="flex flex-col gap-4">
-          <PlaidLink user={user} variant="primary" />
+          {/* <PlaidLink user={user} variant="primary" /> */}
         </div>
       ): (
         <>
